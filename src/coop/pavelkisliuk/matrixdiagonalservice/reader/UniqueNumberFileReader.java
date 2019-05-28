442d14da7c596ff3ad80a142acb93adf8b1d08e9
PavelKisliuk <pavelsergeevichkisliuk2015@mail.ru>
2019-05-28 13:27:01 +0300

    add unique numbers file reader + test

---
+++ src/coop/pavelkisliuk/matrixdiagonalservice/reader/UniqueNumberFileReader.java
@@ -0,0 +1,42 @@
+package coop.pavelkisliuk.matrixdiagonalservice.reader;
+
+import coop.pavelkisliuk.matrixdiagonalservice.exception.CustomException;
+import org.apache.logging.log4j.Level;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class UniqueNumberFileReader {
+	private static final Logger LOGGER = LogManager.getLogger();
+
+	public String read(String path) throws CustomException {
+		if (path == null ||
+				path.isEmpty() ||
+				path.isBlank() ||
+				Files.notExists(Paths.get(path))) {
+			throw new CustomException();
+		}
+		//----------------------------------------------------------------
+		String data;
+		try (BufferedReader reader = Files.newBufferedReader(Paths.get(path))) {
+			LOGGER.log(Level.TRACE, "Start to read data from file.");
+			//----------------------------------------------------------------
+			data = reader.readLine();
+			//----------------------------------------------------------------
+			LOGGER.log(Level.DEBUG, "Data red from file.");
+		} catch (FileNotFoundException e) {
+			LOGGER.log(Level.ERROR, "File didn't find", e);
+			return "";
+		} catch (IOException e) {
+			LOGGER.log(Level.ERROR, "Can't read file", e);
+			return "";
+		}
+		//----------------------------------------------------------------
+		return data;
+	}
+}
--- test/coop/pavelkisliuk/matrixdiagonalservice/reader/UniqueNumberFileReaderTest.java
+++ test/coop/pavelkisliuk/matrixdiagonalservice/reader/UniqueNumberFileReaderTest.java
@@ -3,13 +3,10 @@ package coop.pavelkisliuk.matrixdiagonalservice.reader;
 import coop.pavelkisliuk.matrixdiagonalservice.exception.CustomException;
 import org.testng.annotations.Test;
 
-import java.util.ArrayList;
-import java.util.List;
+import static org.testng.Assert.assertEquals;
 
-import static org.testng.Assert.*;
-
-public class MatrixFileReaderTest {
-	private MatrixFileReader fileReader = new MatrixFileReader();
+public class UniqueNumberFileReaderTest {
+	private UniqueNumberFileReader fileReader = new UniqueNumberFileReader();
 
 	@Test(expectedExceptions = CustomException.class)
 	public void testReadNull() throws CustomException {
@@ -28,11 +25,8 @@ public class MatrixFileReaderTest {
 
 	@Test
 	public void testRead() throws CustomException {
-		List<String> expected = new ArrayList<>();
-		for(int i = 0; i < 10; i++) {
-			expected.add("0-0-0-0-0-0-0-0-0-0");
-		}
+		String expected = "4-16-64-256-1024";
 
-		assertEquals(fileReader.read("testfile/Square_Matrix.txt"), expected);
+		assertEquals(fileReader.read("testfile/Unique_Numbers.txt"), expected);
 	}
 }
